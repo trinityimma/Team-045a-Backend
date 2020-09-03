4c8fc32e24ce96eef1e4a5cbed1aa7af4a85ad91
unknown <igagapeter477@gmail.com>
2020-09-03 14:09:25 +0100

    routes for register and login created

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
+
+const { home } = require('../controllers/index');
 const controllers = require('../controllers/index');
 
 
-router.get('/', controllers.home);
+router.get('/', home);
 
-router.get('/register', controllers.register_get);
+router.get('/register',controllers.register_get);
 
 router.post('/register', controllers.register_post);
+
+router.get('/login', controllers.login_get);
+
+router.post('/login', controllers.login_post);
 
 module.exports = router;
